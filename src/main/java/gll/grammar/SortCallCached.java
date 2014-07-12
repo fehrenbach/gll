4791a99d5360d700ef34d21e6bfc5aec0cf6fb72
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2014-07-12 13:07:48 +0200

    Unsuccessful attempt to fix a Frame escapes error.

--- src/main/java/gll/grammar/SortCallCached.java
+++ src/main/java/gll/grammar/SortCallCached.java
@@ -5,25 +5,41 @@ import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.api.nodes.InvalidAssumptionException;
+import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RootNode;
 import gll.gss.Stack;
 import gll.parser.State;
 
 public class SortCallCached extends Symbol {
+    class ArgumentNode<T> extends Node {
+
+        private final int i;
+
+        public ArgumentNode(int i) {
+            this.i = i;
+        }
+
+        T execute(VirtualFrame truffleFrame) {
+            return (T) truffleFrame.getArguments()[i];
+        }
+    }
     class SortCallRootNode extends RootNode {
         @Children private final Production[] alternatives;
+        @Child private ArgumentNode<State> stateArgument;
+        @Child private ArgumentNode<Stack> frameArgument;
 
         SortCallRootNode(Production[] alternatives) {
             this.alternatives = alternatives;
+            this.stateArgument = new ArgumentNode<State>(0);
+            this.frameArgument = new ArgumentNode<Stack>(1);
             adoptChildren();
         }
 
         @Override
         public Object execute(VirtualFrame truffleFrame) {
-            Object[] arguments = truffleFrame.getArguments();
-            State state = (State) arguments[0];
-            Stack frame = (Stack) arguments[1];
-            for (final Production production : alternatives) {
+            State state = stateArgument.execute(truffleFrame);
+            Stack frame = frameArgument.execute(truffleFrame);
+            for (Production production : alternatives) {
                 production.schedule(truffleFrame, state, frame);
             }
             return null;
